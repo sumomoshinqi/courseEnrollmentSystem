d4874a4ae7b14a0168d5b412d39d0df789af37cb
EdamLee <edam.lqq@gmail.com>
2015-05-15 00:38:28 +0800

    enroll module

--- enroll/enroll.c
+++ enroll/enroll.c
@@ -17,11 +17,17 @@ void finishWithError(MYSQL *mysql)
 
 int enroll(MYSQL *mysql)
 {
-    MYSQL_RES *result = NULL;
+    MYSQL_RES *result = NULL, *resultEnroll = NULL;
+    MYSQL_ROW row, rowEnroll;
     
     unsigned long loginName;
-    char query[100];
+    char query[200];
     char courseID[20];
+    char timeSlot[30];
+    char tmp[5];
+    int i, j, weekday, courseStart, courseEnd;
+    int weekdayEnroll, courseStartEnroll, courseEndEnroll;
+    
     // Read loginName, loginPassword from loginInfo.dat
     freopen("loginInfo.dat", "r", stdin);
     scanf("%ld", &loginName);
@@ -29,7 +35,72 @@ int enroll(MYSQL *mysql)
     freopen("enrollInfo.dat", "r", stdin);
     scanf("%s", courseID);
     
-    // Test if the there's vacancy
+    // Check if time slots are available
+    memset(query, 0, sizeof(query));
+    sprintf(query, "select courseTimeslot from CourseList where courseID=\"%s\";", courseID);
+    if (mysql_query(mysql, query)) {
+        finishWithError(mysql);
+    }
+    result = mysql_store_result(mysql);
+    if (!mysql_num_rows(result)) {
+        // Invalid course ID
+        return -2;
+    } else {
+        // Get timeslot
+        while ((row = mysql_fetch_row(result))) {
+            strcpy(timeSlot, row[0]);
+            
+            weekday = timeSlot[0] - '0';
+            for (i = 2, j = 0; timeSlot[i] >= '0' && timeSlot[i] <= '9'; i++) {
+                tmp[j++] = timeSlot[i];
+            }
+            tmp[j] = '\0';
+            courseStart = atoi(tmp);
+            
+            ++i;
+            for (j = 0; timeSlot[i] >= '0' && timeSlot[i] <= '9'; i++) {
+                tmp[j++] = timeSlot[i];
+            }
+            tmp[j] = '\0';
+            courseEnd = atoi(tmp);
+            
+            // Check if timeslot is available
+            // Compare with courses in CourseEnrollment
+            memset(query, 0, sizeof(query));
+            sprintf(query, "SELECT courseTimeslot FROM CourseEnrollment, CourseList WHERE CourseEnrollment.course = CourseList.courseID AND CourseEnrollment.student = \"%ld\";", loginName);
+            if (mysql_query(mysql, query)) {
+                finishWithError(mysql);
+            }
+            resultEnroll = mysql_store_result(mysql);
+            
+            while ((rowEnroll = mysql_fetch_row(resultEnroll))) {
+                strcpy(timeSlot, rowEnroll[0]);
+                
+                weekdayEnroll = timeSlot[0] - '0';
+                for (i = 2, j = 0; timeSlot[i] >= '0' && timeSlot[i] <= '9'; i++) {
+                    tmp[j++] = timeSlot[i];
+                }
+                tmp[j] = '\0';
+                courseStartEnroll = atoi(tmp);
+                
+                ++i;
+                for (j = 0; timeSlot[i] >= '0' && timeSlot[i] <= '9'; i++) {
+                    tmp[j++] = timeSlot[i];
+                }
+                tmp[j] = '\0';
+                courseEndEnroll = atoi(tmp);
+                
+                // Time conflict
+                if ((weekday == weekdayEnroll) && !(courseEnd < courseStartEnroll || courseStart > courseEndEnroll)) {
+                    return -3;
+                }
+            }
+        }
+        
+    }
+    
+    
+    // Check if the there's vacancy
     memset(query, 0, sizeof(query));
     sprintf(query, "select * from CourseList where courseVacancy > courseTaken and courseID=\"%s\";", courseID);
     if (mysql_query(mysql, query)) {
@@ -92,6 +163,10 @@ int main(int argc, const char * argv[]) {
         printf("选课成功");
     } else if (enrollStateCode == -1) {
         printf("已经选择该课程");
+    } else if (enrollStateCode == -2) {
+        printf("无效课程号");
+    } else if (enrollStateCode == -3) {
+        printf("时间冲突，已选择其他课程");
     }
     
     // Write login state into loginState.dat
@@ -102,6 +177,10 @@ int main(int argc, const char * argv[]) {
         printf("选课成功");
     } else if (enrollStateCode == -1) {
         printf("已经选择该课程");
+    } else if (enrollStateCode == -2) {
+        printf("无效课程号");
+    } else if (enrollStateCode == -3) {
+        printf("时间冲突，已选择其他课程");
     }
 
     mysql_close(mysql);
